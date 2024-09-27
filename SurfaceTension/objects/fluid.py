e01242d2ad54f897a3dcce92b36526a19f9797ae
zcszcs522 <x-gao22@mails.tsinghua.edu.cn>
2024-09-27 23:26:56 +0800

    use utils in files

--- SurfaceTension/objects/fluid.py
+++ SurfaceTension/objects/fluid.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import utils
 
 class fluid_object():
     def __init__(self, NX, NY, NZ):
@@ -9,38 +10,7 @@ class fluid_object():
         self.u = np.zeros((NX+1, NY, NZ))  # 流体速度定义在面中心
         self.p = np.zeros((NX, NY, NZ))    # 流体压力定义在单元中心
 
-    def div(self):
-        """计算散度"""
-        divsp = np.zeros((self.NX, self.NY, self.NZ))
-        # x方向
-        for i in range(self.NX):
-            for j in range(self.NY):
-                for k in range(self.NZ):
-                    if i == 0:
-                        divsp[i, j, k] += self.u[i+1, j, k] - self.u[i, j, k]
-                    elif i == self.NX-1:
-                        divsp[i, j, k] -= self.u[i, j, k] - self.u[i-1, j, k]
-                    else:
-                        divsp[i, j, k] += (self.u[i+1, j, k] - self.u[i-1, j, k]) / 2.0
-        # y方向和z方向类似...
-        return divsp
-
-    def grad(self):
-        """计算梯度"""
-        gradp = np.zeros((self.NX+1, self.NY+1, self.NZ+1))
-        # x方向
-        for i in range(self.NX):
-            for j in range(self.NY):
-                for k in range(self.NZ):
-                    if i == 0:
-                        gradp[i, j, k] = (self.p[i, j, k] - self.p[i+1, j, k]) / 2.0
-                    elif i == self.NX-1:
-                        gradp[i, j, k] = (self.p[i, j, k] - self.p[i-1, j, k]) / 2.0
-                    else:
-                        gradp[i, j, k] = (self.p[i+1, j, k] - self.p[i-1, j, k]) / 2.0
-        # y方向和z方向类似...
-        return gradp
-
+    
     def pressure_solve(self):
         """简单的压力求解，使用雅可比迭代"""
         tolerance = 1e-6
@@ -72,7 +42,7 @@ class fluid_object():
 
     def update_velocity_field(self):
         """更新速度场"""
-        u_p = self.grad()
+        u_p = utils.grad()
         self.u -= u_p
 
     def run_simulation(self):
@@ -96,7 +66,7 @@ def main():
     NX = 100
     NY = 100
     NZ = 100
-    simulation = FluidSimulation(NX, NY, NZ)
+    simulation = fluid_object(NX, NY, NZ)
     simulation.u[:, :, :] = 1.0  # 模拟一些流体运动
     simulation.run_simulation()
     simulation.visualize_velocity_field()
