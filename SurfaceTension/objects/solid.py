Fixing implementing error in O(t)^4 precision, adding Tennis racket effect demo video

SurfaceTension/objects/solid.py
@@ -19,8 +19,12 @@ class SolidObject():
         - ang_velo (np.ndarray): Angular velocity.
         - obj_file (str): Path to the .obj file.
         - is_first_frame (bool): Is the first frame or not. 
-        - acc_last_frame (np.ndarray): Linear velocity of last frame.
-        - ang_acc_last_frame (np.ndarray): Angular velocity of last frame.
+        - centroid_last_frame (np.ndarray): Position of the centroid of last frame.
+        - rotation_matrix_last_frame (np.ndarray): Rotation matrix of last frame. 
+        - velo_last_frame (np.ndarray): Linear velocity of last frame.
+        - ang_velo_last_frame (np.ndarray): Angular velocity of last frame.
+        - acc_last_frame (np.ndarray): Linear acceleration of last frame.
+        - ang_acc_last_frame (np.ndarray): Angular acceleration of last frame.
 
         Note:
         - The inertia tensor is assumed to be a diagonal matrix for computational efficiency, 
@@ -44,6 +48,10 @@ class SolidObject():
         self.mesh = trimesh.load(obj_file)  # Load .obj file
         
         self.is_first_frame = True
+        self.centroid_last_frame = np.array([0,0,0])
+        self.rotation_matrix_last_frame = np.eye(3)
+        self.velo_last_frame = np.array([0,0,0])
+        self.ang_velo_last_frame = np.array([0,0,0])
         self.acc_last_frame = np.array([0,0,0])
         self.ang_acc_last_frame = np.array([0,0,0])
         
@@ -78,9 +86,16 @@ class SolidObject():
     def compute_inertia_inverse_ground(self): 
         return self.rotation_matrix @ self.iner_inv @ self.rotation_matrix.T
 
-    # def update_kinetic_energy(self): 
-    #     self.energy = 0.5 * (self.mass * self.velo.T @ self.velo + self.ang_velo.T @ self.compute_inertia_ground() @ self.ang_velo)
-    #     print("Energy: ", self.energy)
+    def update_kinetic_energy(self): 
+        self.energy = 0.5 * (self.mass * self.velo.T @ self.velo + self.ang_velo.T @ self.compute_inertia_ground() @ self.ang_velo)
+        
+    def update_last_frame_vars(self, acc, ang_acc): 
+        self.centroid_last_frame = self.centroid
+        self.rotation_matrix_last_frame = self.rotation_matrix
+        self.velo_last_frame = self.velo
+        self.ang_velo_last_frame = self.ang_velo
+        self.acc_last_frame = acc
+        self.ang_acc_last_frame = ang_acc
 
     def update(self, time, force, torque):
         # Detect if is the first frame, if so then initialize 
@@ -101,16 +116,16 @@ class SolidObject():
         # Check if this is first frame. 
         # If so, initialize linear acceleration and angular accelaration of last frame. 
         if (self.is_first_frame): 
-            self.is_first_frame = False
-            self.acc_last_frame = lin_acc
-            self.ang_acc_last_frame = ang_acc
+            self.update_last_frame_vars(acc=lin_acc, ang_acc=ang_acc)
 
         # Update linear motion
-        posi_change = self.velo * time + (4 * lin_acc - self.acc_last_frame) * time * time / 3
+        posi_change = self.velo * time + (4 * lin_acc - self.acc_last_frame) * (time ** 2) / 3
         self.centroid += posi_change
+        # self.centroid = (2 * self.centroid - self.centroid_last_frame) + self.velo * time + lin_acc * (time ** 2)
         
         # Update linear velocity
         self.velo += (1.5 * lin_acc - 0.5 * self.acc_last_frame) * time
+        # self.velo = (self.centroid - self.centroid_last_frame) / (time * 2)
         
         # Update rotation matrix 
         eff_ang_velo = self.ang_velo + \
@@ -124,8 +139,13 @@ class SolidObject():
         # Update angular velocity
         self.ang_velo += (1.5 * ang_acc - 0.5 * self.ang_acc_last_frame) * time
         
+        # Update last-frame variables
+        self.update_last_frame_vars(acc=lin_acc, ang_acc=ang_acc)
+        
         # Update energy 
         self.update_kinetic_energy()
+        
+        self.is_first_frame = False
         
 
 

visualization_simple.py
@@ -6,11 +6,12 @@ import SurfaceTension.objects.fluid as fluid
 import SurfaceTension.interaction_of_object.solid_solid as solid_solid
         
 class SolidObjectVisualizer:
-    def __init__(self, solid_objs):
+    def __init__(self, solid_objs, dt):
         # Sorted by increasing order of mass
         self.solid_objs = sorted(solid_objs, key=lambda obj: obj.mass, reverse=False)
+        self.dt = dt
 
-    def visualize(self, steps=3000, dt=0.03, forces=None, torques=None):
+    def visualize(self, steps=1000, forces=None, torques=None):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         ax.set_xlim([-5, 5])
@@ -29,14 +30,12 @@ class SolidObjectVisualizer:
             ax.set_xlim([-3, 3])
             ax.set_ylim([-3, 3])
             ax.set_zlim([-3, 3])
-
-            # print("Energy: ", self.solid_objs[1].energy)
             
             for i, solid_obj in enumerate(self.solid_objs):
                 vertices, faces = solid_obj.get_mesh_data()
                 # Update each object's state
-                solid_obj.update(dt, (forces[i] - 1 * solid_obj.centroid + np.array([0, 0, -0.981]) * solid_obj.mass) * 0, - 0.01 * solid_obj.ang_velo + torques[i])
-                # print("Energy: ", self.solid_objs[1].energy)
+                solid_obj.update(self.dt, (forces[i] - 1 * solid_obj.centroid + np.array([0, 0, -0.981]) * solid_obj.mass) , - 0.00 * solid_obj.ang_velo + torques[i])
+                # print("Energy: ", solid_obj.energy)
                 
                 for j in range(i - 1, -1, -1):
                     # check and handle collision
@@ -63,7 +62,7 @@ class SolidObjectVisualizer:
                               axis[0], axis[1], axis[2],
                               color=['r', 'g', 'b'][j], length=1.0)
 
-            plt.pause(0.003)
+            plt.pause(self.dt)
 
         plt.show()
 
@@ -77,7 +76,7 @@ solid_obj_1 = solid.SolidObject(
     axis=np.array([1.0, 0.0, 0.0]),
     angle=0.0,
     velo=np.array([0.0, 0.0, 0.0]),
-    ang_velo=np.array([0.0, 0.0, 0.0]),
+    ang_velo=np.array([0.0, -2.0, 0.0]),
     obj_file="./data/icosahedron_input.obj"
 )
 
@@ -85,17 +84,17 @@ solid_obj_2 = solid.SolidObject(
     name="TestSolid2",
     mass=1.0,
     volume=1.0,
-    iner=np.array([25.0, 5.0, 1.0]),
+    iner=np.array([1.0, 1.0, 1.0]),
     centroid=np.array([0.0, 0.0, -1.5]),
     axis=np.array([1.0, 0.0, 0.0]),
     angle=0.0,
     velo=np.array([0.0, 0.0, 0.0]),
-    ang_velo=np.array([0.0, 1.0, 0.01]),
+    ang_velo=np.array([0.0, 2.0, 0.01]),
     obj_file="./data/icosahedron_input.obj"
 )
 
 solid_objs = [solid_obj_1, solid_obj_2]
 
-visualizer = SolidObjectVisualizer(solid_objs)
+visualizer = SolidObjectVisualizer(solid_objs, dt=0.05)
 visualizer.visualize()
 