4fb923843b1a821f48dca2112f19a93032533674
zcszcs522 <x-gao22@mails.tsinghua.edu.cn>
2024-11-29 17:48:37 +0800

    change to animation

--- visualization_simple.py
+++ visualization_simple.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 import SurfaceTension.objects.solid as solid
 import SurfaceTension.objects.fluid as fluid
@@ -23,13 +24,23 @@ class SolidObjectVisualizer:
             forces = [np.array([0, 0, 0]) for _ in self.solid_objs]
         if torques is None:
             torques = [np.array([0, 0, 0]) for _ in self.solid_objs]
+            
+        # Create animation
+        ani = animation.FuncAnimation(fig, self.update, frames=steps, interval=self.dt, blit=True, fargs=(ax, forces, torques))
+
+        # Save the animation to a video file
+        ani.save('simple_collision.mp4', writer='ffmpeg', fps=30)
+
         
-        for _ in range(steps):
+        #for _ in range(steps):
+    def update(self, frame, ax, forces, torques):
             
             ax.cla()
             ax.set_xlim([-3, 3])
             ax.set_ylim([-3, 3])
             ax.set_zlim([-3, 3])
+            
+            artists = []
             
             for i, solid_obj in enumerate(self.solid_objs):
                 vertices, faces = solid_obj.get_mesh_data()
@@ -44,27 +55,27 @@ class SolidObjectVisualizer:
 
                 # Plot the centroid
                 centroid = solid_obj.centroid
-                ax.scatter(centroid[0], centroid[1], centroid[2], color='r', s=50, label="Centroid" if i == 0 else None)
+                artists.append(ax.scatter(centroid[0], centroid[1], centroid[2], color='r', s=50, label="Centroid" if i == 0 else None))
 
                 # Draw each face
                 for face in faces:
                     x = vertices[face, 0]
                     y = vertices[face, 1]
                     z = vertices[face, 2]
-                    ax.plot_trisurf(x, y, z, color=(0.7, 0.7, 0.7, 0.5), edgecolor='k', linewidth=0.2)
+                    artists.append(ax.plot_trisurf(x, y, z, color=(0.7, 0.7, 0.7, 0.5), edgecolor='k', linewidth=0.2))
 
                 # Orientation axes
                 rotation_matrix = solid_obj.rotation_matrix
                 # Draw orientation axes for each solid
                 for j in range(3):
                     axis = rotation_matrix[:, j]
-                    ax.quiver(centroid[0], centroid[1], centroid[2],
+                    artists.append(ax.quiver(centroid[0], centroid[1], centroid[2],
                               axis[0], axis[1], axis[2],
-                              color=['r', 'g', 'b'][j], length=1.0)
+                              color=['r', 'g', 'b'][j], length=1.0))
 
-            plt.pause(self.dt)
-
-        plt.show()
+            #plt.pause(self.dt)
+            return artists
+        #plt.show()
 
 # Example: Assuming that solid_obj is your SolidObject instance and provides the getmesh_data method
 solid_obj_1 = solid.SolidObject(
